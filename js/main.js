515c37d1cf80bfc9378c50e3b43c4f8766a1ec94
lghlove92 <mydlrudgus@gmail.com>
2019-04-10 18:13:32 +0900

    last

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 var item = $('.menu-item'); 
-
+var itemSub = $('.sub-menu li a'); 
 item.attr('tabindex','0');
+itemSub.attr('tabindex','0');
 item.on('mouseover focusin',function(){
     item.removeClass('menu-act');
     $(this).addClass('menu-act');
 }); 
-item.on('mouseout focusin',function(){
+$('.sub-menu li:last-child a').on('focusout',function(){
+    item.removeClass('menu-act');
+});
+$('.sub-menu li:last-child a').on('mouseout',function(){
     item.removeClass('menu-act');
 });
